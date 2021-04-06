594b41fd433c0a7a5a6a16d61333b4d64e87a373
suzukalight <mail@mkubara.com>
2021-04-06 19:23:53 +0900

    Update Stack to use mixin

--- src/components/atoms/HStack/index.tsx
+++ src/components/atoms/HStack/index.tsx
@@ -1,25 +1,39 @@
-import React, { ReactNode } from "react";
-import styled from "styled-components/native";
+import React, { ReactNode } from 'react';
+import styled from 'styled-components/native';
 
-export type HStackStyledProps = {
-  w?: number | string;
-  maxW?: number | string;
-};
+import { LayoutProps, layoutMixin } from '../../styles/layout';
+import { sp, SpaceProps, Space, spaceMixin } from '../../styles/space';
+import { ColorProps } from '../../styles/color';
+import { BackgroundProps, backgroundMixin } from '../../styles/background';
 
-export const HStackStyled = styled.View`
+export type HStackStyledProps = Partial<LayoutProps> &
+  Partial<SpaceProps> &
+  Partial<ColorProps> &
+  Partial<BackgroundProps>;
+
+export const HStackStyled = styled.View<HStackStyledProps>`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: stretch;
 
-  width: ${({ w }: HStackStyledProps) => w ?? "auto"};
-  max-width: ${({ maxW }: HStackStyledProps) => maxW ?? "auto"};
+  ${layoutMixin}
+  ${spaceMixin}
+  ${backgroundMixin}
 `;
 
 export type HStackProps = HStackStyledProps & {
   children?: ReactNode;
+  spacing?: Space;
 };
 
-export const HStack = ({ children, ...styles }: HStackProps) => (
-  <HStackStyled children={children} {...styles} />
-);
+export const HStack = ({ children, spacing, ...styles }: HStackProps) => {
+  const childrenWithProps = spacing
+    ? React.Children.map(children, (child) => {
+        if (!React.isValidElement(child)) return child;
+        return React.cloneElement(child, { style: { marginTop: sp(spacing) } });
+      })
+    : children;
+
+  return <HStackStyled children={childrenWithProps} {...styles} />;
+};
--- src/components/atoms/VStack/index.tsx
+++ src/components/atoms/VStack/index.tsx
@@ -1,17 +1,39 @@
-import React, { ReactNode } from "react";
-import styled from "styled-components/native";
+import React, { ReactNode } from 'react';
+import styled from 'styled-components/native';
 
-export const VStackStyled = styled.View`
+import { LayoutProps, layoutMixin } from '../../styles/layout';
+import { sp, SpaceProps, Space, spaceMixin } from '../../styles/space';
+import { ColorProps } from '../../styles/color';
+import { BackgroundProps, backgroundMixin } from '../../styles/background';
+
+type VStackStyledProps = Partial<LayoutProps> &
+  Partial<SpaceProps> &
+  Partial<ColorProps> &
+  Partial<BackgroundProps>;
+
+export const VStackStyled = styled.View<VStackStyledProps>`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+
+  ${layoutMixin}
+  ${spaceMixin}
+  ${backgroundMixin}
 `;
 
-export type VStackProps = {
+export type VStackProps = VStackStyledProps & {
   children?: ReactNode;
+  spacing?: Space;
 };
 
-export const VStack = ({ children }: VStackProps) => (
-  <VStackStyled children={children} />
-);
+export const VStack = ({ children, spacing, ...styles }: VStackProps) => {
+  const childrenWithProps = spacing
+    ? React.Children.map(children, (child) => {
+        if (!React.isValidElement(child)) return child;
+        return React.cloneElement(child, { style: { marginTop: sp(spacing) } });
+      })
+    : children;
+
+  return <VStackStyled children={childrenWithProps} {...styles} />;
+};
