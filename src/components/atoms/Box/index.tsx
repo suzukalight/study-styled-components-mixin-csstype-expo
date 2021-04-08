b025816efc188e21f6e195c32dbe045b39e587a8
suzukalight <mail@mkubara.com>
2021-04-08 16:18:23 +0900

    Add Box component

---
+++ src/components/atoms/Box/index.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from 'react';
+import styled from 'styled-components/native';
+
+import { LayoutProps, layoutMixin } from '../../styles/layout';
+import { SpaceProps, spaceMixin } from '../../styles/space';
+import { flexboxMixin, FlexboxProps } from '../../styles/flexbox';
+import { BackgroundProps, backgroundMixin } from '../../styles/background';
+import { borderMixin, BorderProps } from '../../styles/border';
+
+export type BoxStyledProps = Partial<LayoutProps> &
+  Partial<SpaceProps> &
+  Partial<FlexboxProps> &
+  Partial<BorderProps> &
+  Partial<BackgroundProps>;
+
+export const BoxStyled = styled.View<BoxStyledProps>`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: stretch;
+
+  ${layoutMixin}
+  ${spaceMixin}
+  ${flexboxMixin}
+  ${backgroundMixin}
+  ${borderMixin}
+`;
+
+export type BoxProps = BoxStyledProps & {
+  children?: ReactNode;
+};
+
+export const Box = (props: BoxProps) => <BoxStyled {...props} />;
