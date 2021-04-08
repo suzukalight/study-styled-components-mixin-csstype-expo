a83559e9344cbe7e9b90a62d1d0c68a5a52d3bca
suzukalight <mail@mkubara.com>
2021-04-08 17:33:27 +0900

    Update Checkbox label

--- src/components/forms/Checkbox/Rhf.tsx
+++ src/components/forms/Checkbox/Rhf.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { ReactNode } from 'react';
 import { FieldValues, Controller, DeepMap, FieldError } from 'react-hook-form';
 
 import { Checkbox, CheckboxCommonProps } from './Checkbox';
@@ -8,39 +8,46 @@ import { Typography } from '../../atoms/Typography';
 import { RhfProps } from '../type';
 import { palette } from '../../styles/color';
 
-export type RhfCheckboxProps<T extends FieldValues> = CheckboxCommonProps & RhfProps<T>;
+export type RhfCheckboxProps<T extends FieldValues> = CheckboxCommonProps &
+  RhfProps<T> & {
+    label?: ReactNode;
+  };
 
 export const RhfCheckbox = <T extends FieldValues>({
   control,
   name,
   rules,
+  label,
   defaultValue,
   ...styles
-}: RhfCheckboxProps<T>) => {
-  const [checked, setChecked] = useState(defaultValue as boolean);
+}: RhfCheckboxProps<T>) => (
+  <Controller
+    control={control}
+    name={name}
+    rules={rules}
+    defaultValue={defaultValue}
+    render={({ field: { onChange, onBlur, value }, formState: { errors } }) => (
+      <VStack>
+        {label && (
+          <Typography w="100%" textAlign="left">
+            {label}
+          </Typography>
+        )}
 
-  return (
-    <Controller
-      control={control}
-      name={name}
-      rules={rules}
-      defaultValue={defaultValue}
-      render={({ field: { onChange, onBlur, value }, formState: { errors } }) => (
-        <VStack>
-          <Checkbox
-            {...styles}
-            value={value as boolean}
-            defaultValue={defaultValue}
-            onBlur={onBlur}
-            onChange={onChange}
-          />
-          {errors[name] && (
-            <Typography w="100%" textAlign="left" color={palette.red}>
-              {(errors[name] as DeepMap<FieldValues, FieldError>)?.message}
-            </Typography>
-          )}
-        </VStack>
-      )}
-    />
-  );
-};
+        <Checkbox
+          {...styles}
+          value={value as boolean}
+          defaultValue={defaultValue}
+          onBlur={onBlur}
+          onChange={onChange}
+        />
+
+        {errors[name] && (
+          <Typography w="100%" textAlign="left" color={palette.red}>
+            {(errors[name] as DeepMap<FieldValues, FieldError>)?.message}
+          </Typography>
+        )}
+      </VStack>
+    )}
+  />
+);
