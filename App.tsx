70ffb67d130bc4c72b0d35749ac5ba4ef2f0c5c6
suzukalight <mail@mkubara.com>
2021-04-15 16:09:12 +0900

    Refactor App

--- App.tsx
+++ App.tsx
@@ -1,33 +1,21 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components';
-import { ThemeProvider as RnElementsThemeProvider } from 'react-native-elements';
+import { ThemeProvider as ReactNativeElementsThemeProvider } from 'react-native-elements';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { themeColors } from './src/components/styles/color';
 import { SignUp } from './src/pages/SignUp';
 
-export default function App() {
-  return (
-    <SafeAreaProvider>
-      <RnElementsThemeProvider>
-        <StyledComponentsThemeProvider theme={themeColors}>
-          <SignUp />
-          <StatusBar style="auto" />
-        </StyledComponentsThemeProvider>
-      </RnElementsThemeProvider>
-    </SafeAreaProvider>
-  );
-}
+export const App = () => (
+  <SafeAreaProvider>
+    <ReactNativeElementsThemeProvider>
+      <StyledComponentsThemeProvider theme={themeColors}>
+        <SignUp />
+        <StatusBar style="auto" />
+      </StyledComponentsThemeProvider>
+    </ReactNativeElementsThemeProvider>
+  </SafeAreaProvider>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    maxWidth: '100%',
-    height: '100%',
-  },
-});
+export default App;
