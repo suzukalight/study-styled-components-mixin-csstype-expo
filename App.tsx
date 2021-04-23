d08d977705f2828ba873dc74f14ff1ad48212860
suzukalight <mail@mkubara.com>
2021-04-23 23:14:53 +0900

    Provide store

--- App.tsx
+++ App.tsx
@@ -3,16 +3,20 @@ import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components';
 import { ThemeProvider as ReactNativeElementsThemeProvider } from 'react-native-elements';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider as ReduxProvider } from 'react-redux';
 
 import { themeColors } from './src/styles/color';
 import { Navigation } from './src/pages/Navigation';
+import { store } from './src/logics/store/store';
 
 export const App = () => (
   <SafeAreaProvider>
     <ReactNativeElementsThemeProvider>
       <StyledComponentsThemeProvider theme={themeColors}>
-        <Navigation />
-        <StatusBar style="auto" />
+        <ReduxProvider store={store}>
+          <Navigation />
+          <StatusBar style="auto" />
+        </ReduxProvider>
       </StyledComponentsThemeProvider>
     </ReactNativeElementsThemeProvider>
   </SafeAreaProvider>
