f85703a36dceefa467c399587ecf923f9d3061c1
suzukalight <mail@mkubara.com>
2021-04-23 23:14:39 +0900

    Add store

---
+++ src/logics/store/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
+
+import rootReducer from '../reducers';
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
